e48ff39cb62cf643e6c5db46c0445e1056d9d833
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2023-01-17 17:03:44 +0200

    4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -40,8 +40,18 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() === decodedToken.id) {
+    await Blog.deleteOne(blog)
+    return response.status(204).end()
+  }
+  else {
+    return response.status(401).json({ error: 'unauthorized' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
