4a53dff7870a98d6a91ef39d6ea7a1bd1d369a17
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2023-01-17 16:01:02 +0200

    ex 4.18

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,12 +28,19 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: (body.likes) ? body.likes : 0
+    likes: (body.likes) ? body.likes : 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
