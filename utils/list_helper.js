130d68f0f1f847049ac3092e1a7fb034a0189105
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2024-03-22 14:58:10 +0200

    finished ex 4.6 and 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   blogs = 1
   return blogs
@@ -21,8 +23,39 @@ const favoriteBlog = (blogList) => {
     : blogList.reduce(reducer, 0)
 }
 
+const mostBlogs = (blogList) => {
+
+  const blogsByAuthor = _.countBy(blogList, 'author')
+  const authorBlogCount = _.map(blogsByAuthor, (blogs, author) => ({
+    author: author,
+    blogs: blogs
+  }))
+  const mostBlogsAuthor = _.maxBy(authorBlogCount, (obj) => obj.blogs)
+  return blogList.length === 0
+    ? 0
+    : mostBlogsAuthor
+}
+
+
+const mostLikes = (blogList) => {
+
+  const blogsByAuthor = _.groupBy(blogList, 'author')
+  const likesByAuthor = _.map(blogsByAuthor, (value, key) => ({
+    author: key,
+    likes: _.sumBy(value, 'likes'),
+  }
+  ))
+  const mostLikedAuthor = _.maxBy(likesByAuthor, 'likes')
+  return blogList.length === 0
+    ? 0
+    : mostLikedAuthor
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
